fb8f1a0c597a41decaf143a1eef32a205893d0a3
Guiribei <guilherme_f_ribeiro@hotmail.com>
2022-12-27 14:41:40 +0100

    feat(parser): add output redirect (not tested)

--- sources/parser/command.c
+++ sources/parser/command.c
@@ -6,7 +6,7 @@
 /*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 19:01:13 by guribeir          #+#    #+#             */
-/*   Updated: 2022/12/27 02:09:41 by guribeir         ###   ########.fr       */
+/*   Updated: 2022/12/27 13:37:57 by guribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,19 @@ static void	open_input_file(t_cmd	*cmds, char *file)
 		printf("minishell: open failed");
 }
 
-//static void	open_output_file(t_cmd	*cmds, char *file)
-//{
-	//cmds.fd_out = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	//if (cmds->fd_in == -1)
-		//perror_handler(pipex->argv[pipex->argc - 1], ": ", 1, minishell);
-//}
+static void	open_output_file(t_cmd	*cmds, char *file)
+{
+	cmds->fd_out = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (cmds->fd_out == -1)
+		printf("minishell: open failed");
+}
+
+static void	open_append_file(t_cmd	*cmds, char *file)
+{
+	cmds->fd_out = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	if (cmds->fd_out == -1)
+		printf("minishell: open failed");
+}
 
 int	count_args(t_token *tokens)
 {
@@ -65,8 +72,24 @@ t_cmd	*init_cmd_table(t_token *tokens)
 				i += 2;
 			open_input_file(&cmds[i - 1], tokens[i + 1].name);
 		}
-		else
-			cmds[0].fd_in = 0;
+		else if ((cmp(tokens[i].name, ">") || cmp(tokens[i].name, ">>")) && i == 0)
+		{
+			while (cmp(tokens[i + 2].name, ">") || cmp(tokens[i + 2].name, ">>"))
+				i += 2;
+			if (cmp(tokens[i].name, ">"))
+				open_output_file(&cmds[i], tokens[i + 1].name);
+			else
+				open_append_file(&cmds[i], tokens[i + 1].name);
+		}
+		else if (cmp(tokens[i].name, ">") || cmp(tokens[i].name, ">>"))
+		{
+			while (cmp(tokens[i + 2].name, ">") || cmp(tokens[i + 2].name, ">>"))
+				i += 2;
+			if (cmp(tokens[i].name, ">"))
+				open_output_file(&cmds[i - 1], tokens[i + 1].name);
+			else
+				open_append_file(&cmds[i - 1], tokens[i + 1].name);
+		}
 		i++;
 	}
 	return(cmds);
