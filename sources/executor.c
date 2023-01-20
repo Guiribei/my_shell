a699112e3d3dc67805810b99bb34945e04081f18
etachott <etachott@student.42sp.org.br>
2023-01-20 19:49:04 -0300

    feat(builtin fork): Builtins are now forked, but hangs on pipe

--- sources/executor.c
+++ sources/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: guribeir <guribeir@student.42.rio>         +#+  +:+       +#+        */
+/*   By: etachott < etachott@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 21:00:15 by guribeir          #+#    #+#             */
-/*   Updated: 2023/01/20 18:32:28 by guribeir         ###   ########.fr       */
+/*   Updated: 2023/01/20 19:45:27 by etachott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,43 @@ void	select_inout(t_cmd *cmds, int i)
 	if (cmds[i].where_read == FILE_IN)
 	{
 		if(dup2(cmds[i].fd_in, 0) == -1)
-			printf("dup failed\n");
+			printf("file in dup failed\n");
 	}
 	if (cmds[i].where_read == PIPE_0)
 	{
+		printf("FD IN = %d | FD OUT = %d\n", cmds[i].fd_in, cmds[i].fd_out);
 		if(dup2(cmds[i - 1].pipe[0], 0) == -1)
-			printf("dup failed\n");
+			printf("pipe 0 dup failed\n");
 	}
 	if (cmds[i].where_write == PIPE_1)
 	{
 		if(dup2(cmds[i].pipe[1], 1) == -1)
-			printf("dup failed\n");
+			printf("pipe 1 dup failed\n");
 	}
 	if (cmds[i].where_write == FILE_OUT)
 	{
 		if(dup2(cmds[i].fd_out, 1) == -1)
-			printf("dup failed\n");
+			printf("file out dup failed\n");
 	}
 }
 
 static void	child(t_cmd *cmds, char **envp, int i)
 {
+	int	ret;
+
+	ret = 0;
 	select_inout(cmds, i);
 	full_close(cmds);
 	if (cmds[i].cmds == NULL || cmds[i].path_cmd == NULL)
 		printf("Command not found, child\n");
-	if (execve(cmds[i].path_cmd, cmds[i].cmds, envp) == -1)
-		printf("faild execve");
+	if (is_builtin(cmds[i].cmds))
+		builtin_run(cmds[i].cmds, envp);
+	else
+	{
+		ret = execve(cmds[i].path_cmd, cmds[i].cmds, envp);
+		if (ret == -1)
+			printf("faild execve");
+	}
 }
 
 static int	parent(t_cmd *cmds)
@@ -80,12 +90,12 @@ int	core(t_cmd *cmds, char **envp)
 	i = 0;
 	while (cmds[i].cmd)
 	{
-		if (is_builtin(cmds[i].cmds))
+		/*if (is_builtin(cmds[i].cmds))
 		{
 			builtin_run(cmds[i].cmds, envp);
 			i++;
 			continue ;
-		}
+		}*/
 		paths = get_paths(envp);
 		cmds[i].path_cmd = find_command(cmds[i].cmds[0], paths);
 		strsclear(paths);
